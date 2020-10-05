7fa6ca0e998068d4b8ee10c3a45a9a064ef35336
Tomas Janousek <tomi@nomi.cz>
2020-10-05 16:14:31 +0200

    Convert to modern Python packaging

---
+++ src/gcal_sync/cli.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="TODO"
+    )
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+    _ = args
